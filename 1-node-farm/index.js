d56f8c9b05b4180345b0ac59277019e521731657
Bader <muhammad.bader@dvsum.com>
2023-08-25 19:41:05 +0500

    Node-Farm: Section Completed

---
+++ 1-node-farm/index.js
@@ -0,0 +1,58 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cards = dataObj?.map((el) => replaceTemplate(tempCard, el)).join("");
+
+    res.end(tempOverview.replace(/{%PRODUCT_CARDS%}/, cards));
+
+    // Product page
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    const productTemplate = replaceTemplate(tempProduct, product);
+    res.end(productTemplate);
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
